ebf49ea910fcbe4bc062db9740179c153bd7c214
Jesus Machado <78329197+Pitiyanky@users.noreply.github.com>
2023-12-01 15:43:21 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -9,15 +9,19 @@
 int main() {
   // Crear hilos para jugadores y jefe de mesa
   int i, num_jugador[MAX_JUGADORES];
-  pthread_t tid_jugador[MAX_JUGADORES], tid_jefe_de_mesa;
+  pthread_t tid_jugador[MAX_JUGADORES];
   for (i = 0; i < MAX_JUGADORES; i++) {
     num_jugador[i] = i;  // Asignar el número del jugador
     pthread_create(&tid_jugador[i], NULL, jugador, &num_jugador[i]);
   }
-  pthread_create(&tid_jefe_de_mesa, NULL, jefe_de_mesa, NULL);
+  pthread_t hilo_jefe_reordenar, hilo_jefe_cartas;
+
+  pthread_create(&hilo_jefe_reordenar, NULL, jefe_de_mesa_reordenar, NULL);
+  pthread_create(&hilo_jefe_cartas, NULL, jefe_de_mesa_cartas, NULL);
 
   // Esperar a que los hilos terminen
-  pthread_join(tid_jefe_de_mesa, NULL);
+  pthread_join(hilo_jefe_reordenar, NULL);
+  pthread_join(hilo_jefe_cartas, NULL);
   for (i = 0; i < MAX_JUGADORES; i++) {
      pthread_join(tid_jugador[i], NULL);
   }
